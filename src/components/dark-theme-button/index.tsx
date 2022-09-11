f032479ae5a02918ea1d2b92df373132705d7c29
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-09-11 12:22:58 -0500

    feat: 🎸 add dark-theme-button component

---
+++ src/components/dark-theme-button/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Button, useColorMode } from "@chakra-ui/react";
+import { HiOutlineSun } from "react-icons/hi";
+import { RiMoonClearFill } from "react-icons/ri";
+
+export function DarkThemeButton() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Button
+      size={{ base: "sm", sm: "md" }}
+      onClick={toggleColorMode}
+      colorScheme={colorMode === "light" ? "blue" : "gray"}
+    >
+      {colorMode === "light" ? <RiMoonClearFill /> : <HiOutlineSun />}
+    </Button>
+  );
+}
