1baf6d61bfc38e47623b533362c57f3a6d62f34c
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-09-14 23:40:54 -0500

    feat: 🎸 configure apollo-client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,33 @@
 import "../styles/globals.css";
+import * as React from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { mainTheme } from "~/themes/main";
 import { Navbar } from "~/components/navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = React.useMemo(() => {
+    const cache = new InMemoryCache();
+
+    if (pageProps.apolloClientState) {
+      cache.restore(pageProps.apolloClientState);
+    }
+
+    const client = new ApolloClient({
+      uri: process.env.API_URI,
+      cache: cache,
+    });
+
+    return client;
+  }, [pageProps.apolloClientState]);
+
   return (
     <ChakraProvider theme={mainTheme}>
-      <Navbar />
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Navbar />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ChakraProvider>
   );
 }
