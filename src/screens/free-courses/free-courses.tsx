f06b2ecb5e7bb450d55bf68ffea95bc3a4bb3628
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2025-05-21 10:45:37 -0500

    refactor: migrate FreeCoursesScreen to use @tanstack/react-query for data fetching

--- src/screens/free-courses/free-courses.tsx
+++ src/screens/free-courses/free-courses.tsx
@@ -1,22 +1,23 @@
-import { useQuery } from "@apollo/client";
 import { Alert, AlertIcon, AlertTitle, Spinner } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 
 import { Introduction } from "~/components/introduction";
 import Meta from "~/components/meta";
-import { freeCoursesQuery } from "~/gql/queries";
 import { MainLayout } from "~/layouts";
-import { FreeCoursesQueryResponsePayload } from "~/typings/free-courses";
+import { getFreeCourses } from "~/services/free-courses";
 
 import { Content } from "./components/content";
 import { FreeCoursesSearchBox } from "./components/free-courses-search-box";
 
 export function FreeCoursesScreen() {
-  const { data, loading, error } =
-    useQuery<FreeCoursesQueryResponsePayload>(freeCoursesQuery);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["freeCourses"],
+    queryFn: getFreeCourses,
+  });
 
-  const freeCourses = data?.freeCourses ?? [];
+  const freeCourses = data ?? [];
 
-  if (loading) {
+  if (isLoading) {
     return (
       <MainLayout>
         <Spinner size="lg" />
@@ -46,8 +47,8 @@ export function FreeCoursesScreen() {
         formId="mJqxB4"
         formLabel="Envía tu sugerencia de un curso aquí."
       />
-      <FreeCoursesSearchBox />
-      <Content />
+      <FreeCoursesSearchBox data={freeCourses} />
+      <Content data={freeCourses} />
     </MainLayout>
   );
 }
