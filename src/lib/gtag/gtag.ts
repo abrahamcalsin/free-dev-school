6433d040e85f0e9b477cc33d4c8b8ebe85652f71
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2025-05-21 10:44:59 -0500

    fix: ensure gtag functions only execute in browser environment

--- src/lib/gtag/gtag.ts
+++ src/lib/gtag/gtag.ts
@@ -4,16 +4,22 @@ type gtagOptions = {
   params: string;
 };
 
+import env from "~/utils/env";
+
 export const pageview = (props: gtagOptions) => {
   const { url } = props;
 
-  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("config", env.GOOGLE_ANALYTICS_ID, {
+      page_path: url,
+    });
+  }
 };
 
 export const event = (options: gtagOptions) => {
   const { action, params } = options;
 
-  window.gtag("event", action, params);
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("event", action, params);
+  }
 };
